6ecfc4aefb96378ec019f0e14736cafc311dd7dc
lzosi <lzosi@126.com>
2014-12-02 16:08:55 +0800

    Update MainActivity.java

--- GitDemo/src/com/example/gitdemo/MainActivity.java
+++ GitDemo/src/com/example/gitdemo/MainActivity.java
@@ -11,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        
+        //1
     }
 }
